24ebac3bed3915a7a38ccadc2a8b67b082f4ed0c
Ale Figueroa <yknx.4.b@gmail.com>
2017-10-15 20:03:49 -0500

    Fix for NaN

--- core/linux/volume.js
+++ core/linux/volume.js
@@ -1,7 +1,7 @@
 const { log } = Math;
 
 module.exports = function volume(val) {
-  const v = parseInt(2000 * log(Math.abs(100 - val) / 100));
+  const v = parseInt(2000 * log(Math.abs(101 - val) / 100));
   if (v > 0) return 0;
   if (v < -4000) return -4000;
   return v;
