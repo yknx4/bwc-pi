4e1062c938ab0e1bed2265c29808bd86c02d4af3
Ale Figueroa <yknx.4.b@gmail.com>
2017-10-15 21:27:57 -0500

    Don't use disabled ones

--- core/start.js
+++ core/start.js
@@ -17,7 +17,9 @@ const netIntensity = net => parseFloat(net.signal_level);
 
 async function checkNetwork(net) {
   const devices = await Device.getAll();
-  const allowedSsids = devices.map(e => e.serialized.ssid);
+  const allowedSsids = devices
+    .filter(e => e.serialized.enabled)
+    .map(e => e.serialized.ssid);
   const blindNearby = allowedSsids.some(ssid => net.ssid === ssid);
   return blindNearby;
 }
