10d493ad813f6ef16105d1c7cc14ceead28e5347
Ale Figueroa <yknx.4.b@gmail.com>
2017-10-15 19:49:15 -0500

    Fix volume in linux

--- core/linux/volume.js
+++ core/linux/volume.js
@@ -1,5 +1,7 @@
 const { log } = Math;
 
 module.exports = function volume(val) {
-  return 100 - parseInt(2000 * log(Math.abs(val) / 100));
+  const v = parseInt(2000 * log(Math.abs(100 - val) / 100));
+  if (v > 0) return 0;
+  if (v < -4000) return -4000;
 };
