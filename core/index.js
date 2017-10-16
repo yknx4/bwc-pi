9d2feaf3cf0eedb3230e64cd46f1c7c6d7b998cb
Ale Figueroa <yknx.4.b@gmail.com>
2017-10-15 19:36:08 -0500

    Fix volume calculator

--- core/index.js
+++ core/index.js
@@ -1,10 +1,9 @@
 const { platform } = require("os");
-const volumeForNet = require("./volume");
 const { volume, command } = require(`./${platform()}`);
 console.log(`Loading specifics for ${platform()} platform.`);
 
 function playFile(file, dbm) {
-  console.log(`Playing ${file} with ${dbm} => ${volume(volumeForNet(dbm))}`);
+  console.log(`Playing ${file} with ${dbm} => ${volume(dbm)}`);
   return command.replace("%VOLUME%", volume(dbm)).replace("%FILE%", file);
 }
 
--- core/linux/volume.js
+++ core/linux/volume.js
@@ -1,5 +1,5 @@
 const { log } = Math;
 
 module.exports = function volume(val) {
-  return parseInt(2000 * log(Math.abs(val)));
+  return parseInt(2000 * log(Math.abs(val) / 100));
 };
