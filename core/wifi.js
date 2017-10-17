3230c89f8f955d8290982d7416131a0a6a4bef90
Ale Figueroa <yknx.4.b@gmail.com>
2017-10-16 22:38:11 -0500

    CHange variable name

--- core/wifi.js
+++ core/wifi.js
@@ -3,11 +3,11 @@ const { promisify } = require("util");
 
 const exec = promisify(child_process.exec);
 const refresh = "sudo iwlist scan";
-const getNetworks = "nmcli -t -f SIGNAL,SSID,BSSID dev wifi list";
+const getNetworksCmd = "nmcli -t -f SIGNAL,SSID,BSSID dev wifi list";
 
 async function getNetworks() {
   await exec(refresh);
-  const { stdout } = await exec(getNetworks);
+  const { stdout } = await exec(getNetworksCmd);
   console.log(stdout);
 }
 
