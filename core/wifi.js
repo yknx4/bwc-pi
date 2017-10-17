507ca160e9964ace3990caa0c4a05e8b985c79eb
Ale Figueroa <yknx.4.b@gmail.com>
2017-10-16 22:43:04 -0500

    Split result

--- core/wifi.js
+++ core/wifi.js
@@ -5,10 +5,18 @@ const exec = promisify(child_process.exec);
 const refresh = "sudo iwlist scan";
 const getNetworksCmd = "nmcli -t -f SIGNAL,SSID,BSSID dev wifi list";
 
+const nmcliRegex = /(\d*):(.*):(([0-9A-Fa-f]{2}\\:){5}([0-9A-Fa-f]{2}))/;
+const unescape = s => s.replace("\\:", ":").replace("\\\\", "\\");
+
 async function getNetworks() {
   await exec(refresh);
   const { stdout } = await exec(getNetworksCmd);
-  console.log(stdout);
+  const nets = stdout
+    .split("\n")
+    .map(nmcliRegex.exec.bind(nmcliRegex))
+    .map(m => {
+      console.log(m);
+    });
 }
 
 module.exports = getNetworks;
